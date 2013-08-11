82084becc21e41e4513fb0c1f7b5ee60c45c8e66
eduardo <eduardo@ADVN-NOT-DESi3>
2013-08-11 17:21:05 -0300

    Inicial

---
+++ src/sugadordedados/Sugador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sugadordedados;
+
+/**
+ *
+ * @author eduardo
+ */
+public class Sugador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
